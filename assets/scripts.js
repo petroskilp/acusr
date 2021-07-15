55f5c1d478fc1ff8f7ba2a73100a82276c410ab0
Luiz Pedro Petroski <petroskilp@gmail.com>
2021-07-15 10:43:10 -0300

    Updating content

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,27 +1,40 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
-  $('#geneModal').on('show.bs.modal', function (e) {
-    var symbol = $(e.relatedTarget).data('symbol');
-    var name = $(e.relatedTarget).data('name');
-    var ens = $(e.relatedTarget).data('ens');
-    var desc = $(e.relatedTarget).data('desc');
-    var ref = $(e.relatedTarget).data('ref');
-    var ncbi = $(e.relatedTarget).data('ncbi');
-    var uniprot = $(e.relatedTarget).data('uniprot');
-    var genecards = $(e.relatedTarget).data('genecards');
-    var string = $(e.relatedTarget).data('string');
-    var stringimg = $(e.relatedTarget).data('stringimg');
-    $(e.currentTarget).find('#geneSymbol').text(symbol);
-    $(e.currentTarget).find('#gene-name').text(name);
-    $(e.currentTarget).find('#gene-ens').text(ens);
-    $(e.currentTarget).find('#gene-desc').text(desc);
-    $(e.currentTarget).find('#gene-references').text(ref);
-    $(e.currentTarget).find('#gene-ncbi').attr("href", ncbi);
-    $(e.currentTarget).find('#gene-uniprot').attr("href", uniprot);
-    $(e.currentTarget).find('#gene-genecards').attr("href", genecards);
-    $(e.currentTarget).find('#gene-string').attr("href", string);
+  $("#geneModal").on("show.bs.modal", function (e) {
+    var symbol = $(e.relatedTarget).data("symbol");
+    var name = $(e.relatedTarget).data("name");
+    var ens = $(e.relatedTarget).data("ens");
+    var desc = $(e.relatedTarget).data("desc");
+    var refs = $(e.relatedTarget).data("ref");
+    var ncbi = $(e.relatedTarget).data("ncbi");
+    var uniprot = $(e.relatedTarget).data("uniprot");
+    var genecards = $(e.relatedTarget).data("genecards");
+    var string = $(e.relatedTarget).data("string");
+    var stringimg = $(e.relatedTarget).data("stringimg");
 
-    $(e.currentTarget).find('#gene-img').attr("src", stringimg);
-  })
-})
+    var ref = refs.split("\n");
 
+    var ul = document.createElement("ul");
+    for (var i = 0; i <= ref.length - 1; i++) {
+      if (ref[i] != "") {
+        var li = document.createElement("li"); // create li element.
+        li.innerHTML = ref[i]; // assigning text to li using array value.
+        ul.appendChild(li); // append li to ul.
+      }
+    }
+    //console.log(ul);
+
+    $(e.currentTarget).find("#geneSymbol").text(symbol);
+    $(e.currentTarget).find("#gene-name").text(name);
+    $(e.currentTarget).find("#gene-ens").text(ens);
+    $(e.currentTarget).find("#gene-desc").text(desc);
+    $(e.currentTarget).find("#gene-references").empty();
+    $(e.currentTarget).find("#gene-references").append(ul);
+    $(e.currentTarget).find("#gene-ncbi").attr("href", ncbi);
+    $(e.currentTarget).find("#gene-uniprot").attr("href", uniprot);
+    $(e.currentTarget).find("#gene-genecards").attr("href", genecards);
+    $(e.currentTarget).find("#gene-string").attr("href", string);
+
+    $(e.currentTarget).find("#gene-img").attr("src", stringimg);
+  });
+});
